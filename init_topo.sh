dce2bfd96a7bb477880917f88b0d94c0aa13bbeb
Filip Tehlar <ftehlar@cisco.com>
2023-10-13 11:36:36 +0200

    more configs

--- init_topo.sh
+++ init_topo.sh
@@ -1,14 +1,14 @@
- sudo ip netns add hsns
+ sudo ip netns add myns
  sudo ip link add veth_vpp1 type veth peer name vpp1
  sudo ip link add veth_vpp2 type veth peer name vpp2
  sudo ip link set dev vpp1 up
  sudo ip link set dev vpp2 up
- sudo ip link set dev veth_vpp1 up netns hsns
- sudo ip link set dev veth_vpp2 up netns hsns
+ sudo ip link set dev veth_vpp1 up netns myns
+ sudo ip link set dev veth_vpp2 up netns myns
 
 
- sudo ip netns exec hsns brctl addbr hsbr
- sudo ip netns exec hsns brctl addif hsbr veth_vpp1
- sudo ip netns exec hsns brctl addif hsbr veth_vpp2
- sudo ip netns exec hsns ifconfig hsbr up
+ sudo ip netns exec myns brctl addbr hsbr
+ sudo ip netns exec myns brctl addif hsbr veth_vpp1
+ sudo ip netns exec myns brctl addif hsbr veth_vpp2
+ sudo ip netns exec myns ifconfig hsbr up
 
